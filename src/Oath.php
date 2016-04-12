2cd466a6949d1636a709ff9ef742ab83813e0911
Niklas Keller <me@kelunik.com>
2016-04-12 19:18:02 +0200

    Use OO API instead of static methods

--- src/Oath.php
+++ src/Oath.php
@@ -3,7 +3,23 @@
 namespace Kelunik\TwoFactor;
 
 class Oath {
-    public static function generateKey($length = 20) {
+    private $length;
+    private $windowSize;
+
+    public function __construct($length = 8, $windowSize = 30) {
+        if (!is_int($length)) {
+            throw new \InvalidArgumentException("Length must be int");
+        }
+
+        if (!is_int($windowSize)) {
+            throw new \InvalidArgumentException("Window size must be int");
+        }
+
+        $this->length = $length;
+        $this->windowSize = $windowSize;
+    }
+
+    public function generateKey($length = 20) {
         if (!is_int($length)) {
             throw new \InvalidArgumentException("Length must be int");
         }
@@ -15,7 +31,7 @@ class Oath {
         return random_bytes($length);
     }
 
-    public static function generateHotp($key, $counter, $length = 8) {
+    public function generateHotp($key, $counter) {
         if (!is_string($key)) {
             throw new \InvalidArgumentException("Key must be string");
         }
@@ -24,26 +40,70 @@ class Oath {
             throw new \InvalidArgumentException("Counter must be int");
         }
 
-        if (!is_int($length)) {
-            throw new \InvalidArgumentException("Length must be int");
-        }
-
         if (strlen($key) < 16) {
             throw new \InvalidArgumentException("Key too short.");
         }
 
-        if ($length < 6) {
-            throw new \InvalidArgumentException("OATH tokens must be at least 6 digits long.");
-        }
-
         $counter = pack("N*", 0, $counter);
         $rawHmac = hash_hmac("sha1", $counter, $key, true);
-        $oath = self::oathTruncate($rawHmac, $length);
+        $oath = $this->oathTruncate($rawHmac);
 
-        return str_pad($oath, $length, "0", STR_PAD_LEFT);
+        return str_pad($oath, $this->length, "0", STR_PAD_LEFT);
     }
 
-    public static function getTimeWindow($time = null, $windowSize = 30) {
+    public function generateTotp($key, $time = null) {
+        return $this->generateHotp($key, $this->getTimeWindow($time ?: time()));
+    }
+
+    public function verifyHotp($value, $key, $counter) {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException("Value must be string");
+        }
+
+        if (!is_string($key)) {
+            throw new \InvalidArgumentException("Key must be string");
+        }
+
+        if (!is_int($counter)) {
+            throw new \InvalidArgumentException("Counter must be int");
+        }
+
+        return hash_equals($value, $this->generateHotp($key, $counter));
+    }
+
+    public function verifyTotp($value, $key, $grace = 2, $currentTime = null) {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException("Value must be string");
+        }
+
+        if (!is_string($key)) {
+            throw new \InvalidArgumentException("Key must be string");
+        }
+
+        if (!is_int($grace) || $grace < 0 || $grace > 5) {
+            throw new \InvalidArgumentException("Grace must be int and between 0 and 5");
+        }
+
+        $currentTime = $currentTime ?: time();
+
+        if (!is_int($currentTime)) {
+            throw new \InvalidArgumentException("Current time must be int");
+        }
+
+        for ($i = 0; $i <= $grace; $i++) {
+            $hotp = self::generateHotp($key, $this->getTimeWindow($currentTime));
+
+            if (hash_equals($hotp, $value)) {
+                return true;
+            }
+
+            $currentTime -= 30;
+        }
+
+        return false;
+    }
+
+    private function getTimeWindow($time = null, $windowSize = 30) {
         if ($time !== null && !is_int($time)) {
             throw new \InvalidArgumentException("Time must be int");
         }
@@ -52,15 +112,15 @@ class Oath {
             throw new \InvalidArgumentException("Window size must be int");
         }
 
-        $time = $time ?? time();
+        $time = $time ?: time();
 
-        return floor($time / $windowSize);
+        return (int) floor($time / $windowSize);
     }
 
     /**
      * @see https://tools.ietf.org/html/rfc4226#section-5.3
      */
-    private static function oathTruncate($rawHmac, $length) {
+    private function oathTruncate($rawHmac) {
         // Take lower 4 bit as offset
         $offset = ord($rawHmac[19]) & 0x0F;
 
@@ -69,6 +129,6 @@ class Oath {
 
         // Mask first bit due to signed / unsigned modulo operations
         // And extract HOTP value according to OTP_LENGTH
-        return ($p[1] & 0x7FFFFFFF) % 10 ** $length;
+        return ($p[1] & 0x7FFFFFFF) % (10 ** $this->length);
     }
 }
--- test/OathTest.php
+++ test/OathTest.php
@@ -5,14 +5,21 @@ namespace Kelunik\TwoFactor;
 class OathTest extends \PHPUnit_Framework_TestCase {
     const KEY = "12345678901234567890";
 
-    /**
-     * @dataProvider provideRfcTestData
-     */
-    public function test(int $time, string $totp) {
-        $this->assertSame($totp, Oath::generateHotp(self::KEY, Oath::getTimeWindow($time)));
+    /** @var Oath */
+    private $oath;
+
+    public function setUp() {
+        $this->oath = new Oath(8, 30);
     }
 
-    public function provideRfcTestData() {
+    /**
+     * @dataProvider provideRfcTestDataForGeneration
+     */
+    public function testGeneration($time, $totp) {
+        $this->assertSame($totp, $this->oath->generateTotp(self::KEY, $time));
+    }
+
+    public function provideRfcTestDataForGeneration() {
         return [
             [59, "94287082"],
             [1111111109, "07081804"],
@@ -20,6 +27,25 @@ class OathTest extends \PHPUnit_Framework_TestCase {
             [1234567890, "89005924"],
             [2000000000, "69279037"],
             [20000000000, "65353130"],
+        ];
+    }
+
+    /**
+     * @dataProvider provideRfcTestDataForValidation
+     */
+    public function testValidation($time, $totp, $result) {
+        $this->assertSame($result, $this->oath->verifyTotp($totp, self::KEY, 2, $time));
+    }
+
+    public function provideRfcTestDataForValidation() {
+        return [
+            [0, "94287082", false],
+            [30, "94287082", true],
+            [59, "94287082", true],
+            [60, "94287082", true],
+            [89, "94287082", true],
+            [119, "94287082", true],
+            [120, "94287082", false],
         ];
     }
 }
